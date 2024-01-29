781c940cdfb06a560311e731599c0597884b09ef
sumatra-dhimoyee <sumatrad@bu.edu>
2024-01-28 20:03:28 -0500

    div and sub implemented v1

--- polynomial.py
+++ polynomial.py
@@ -19,6 +19,14 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        return repr(self.p1) + " - " + repr(self.p2)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,8 +41,29 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, Add):
+            if isinstance(self.p2, Add):
+                 return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        if isinstance(self.p2, Add):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " / " + repr(self.p2)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
+#x^2+1/x+x^3+3x-2
+poly1= Add(Add(Add(Mul(X(), X()), Div(Int(1), X())), Mul(Mul(X(), X()), X())),Sub(Mul(Int(3), X()), Int(2)))
+print(poly1)
+
+poly2= Sub(Add(Div(Int(1), Mul(X(), X())), Div(Int(2), Mul(X(), Mul(X(), X())))), Mul(X(), Mul(X(), X())))
+print(poly2)
 
